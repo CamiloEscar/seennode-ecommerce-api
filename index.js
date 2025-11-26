5a6b0448f2ab81a20f776bd541fa88f844b9ea64
Camilo Escar <camiloescar1995@gmail.com>
2025-11-26 17:51:29 -0300

    implementacion de cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 
 import express from 'express'
 import {PrismaClient} from './generated/prisma/index.js'
+import cors from "cors";
 
 const app = express()
 const prisma = new PrismaClient()
 
 const PORT = process.env.PORT || 3000
 
+app.use(cors());
 app.get('/productos', async (req, res) => {
 
     const productos = await prisma.producto.findMany();
